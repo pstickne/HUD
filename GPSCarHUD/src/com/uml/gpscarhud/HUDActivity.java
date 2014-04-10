eb80d57699d978c8b2f0fb14d20af37f3bcc0dd4
pstickne <pstick158@gmail.com>
2014-04-09 22:29:23 -0400

    Removed dependability for google play services

--- GPSCarHUD/src/com/uml/gpscarhud/HUDActivity.java
+++ GPSCarHUD/src/com/uml/gpscarhud/HUDActivity.java
@@ -1,26 +1,13 @@
 package com.uml.gpscarhud;
 
-import java.util.ArrayList;
-
-import org.w3c.dom.Document;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.IntentSender;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.maps.model.LatLng;
 import com.uml.gpscarhud.util.SystemUiHider;
 
 /**
@@ -29,9 +16,7 @@ import com.uml.gpscarhud.util.SystemUiHider;
  * 
  * @see SystemUiHider
  */
-public class HUDActivity extends Activity implements
-		GooglePlayServicesClient.ConnectionCallbacks,
-		GooglePlayServicesClient.OnConnectionFailedListener {
+public class HUDActivity extends Activity {
 	/**
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -60,22 +45,6 @@ public class HUDActivity extends Activity implements
 	 */
 	private SystemUiHider mSystemUiHider;
 
-	private static TextView locationText = null;
-	private static TextView directionText = null;
-
-	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
-	LocationClient client = null;
-
-	Location currentLocation;
-
-	/*
-	 * For testing South Campus coords.
-	 */
-
-	private static final double southCampusLat = 42.642786;
-	private static final double southCampusLong = -71.335007;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -84,13 +53,10 @@ public class HUDActivity extends Activity implements
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
-		locationText = (TextView) findViewById(R.id.fullscreen_content);
-		directionText = (TextView) findViewById(R.id.fullscreen_directions);
 
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
+		mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
 		mSystemUiHider.setup();
 		mSystemUiHider
 				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
@@ -121,8 +87,7 @@ public class HUDActivity extends Activity implements
 							// If the ViewPropertyAnimator APIs aren't
 							// available, simply show or hide the in-layout UI
 							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
+							controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
 						}
 
 						if (visible && AUTO_HIDE) {
@@ -147,10 +112,7 @@ public class HUDActivity extends Activity implements
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
-
-		client = new LocationClient(this, this, this);
+		findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 	}
 
 	@Override
@@ -161,20 +123,6 @@ public class HUDActivity extends Activity implements
 		// created, to briefly hint to the user that UI controls
 		// are available.
 		delayedHide(100);
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-		// Connect the client.
-		client.connect();
-	}
-
-	@Override
-	protected void onStop() {
-		// Disconnecting the client invalidates it.
-		client.disconnect();
-		super.onStop();
 	}
 
 	/**
@@ -207,56 +155,5 @@ public class HUDActivity extends Activity implements
 	private void delayedHide(int delayMillis) {
 		mHideHandler.removeCallbacks(mHideRunnable);
 		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
-
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		if (connectionResult.hasResolution()) {
-			try {
-				// Start an Activity that tries to resolve the error
-				connectionResult.startResolutionForResult(this,
-						CONNECTION_FAILURE_RESOLUTION_REQUEST);
-				/*
-				 * Thrown if Google Play services canceled the original
-				 * PendingIntent
-				 */
-			} catch (IntentSender.SendIntentException e) {
-				// Log the error
-				e.printStackTrace();
-			}
-		} else {
-			/*
-			 * If no resolution is available, display a dialog to the user with
-			 * the error.
-			 */
-			locationText.setText("Error");
-		}
-
-	}
-
-	@Override
-	public void onConnected(Bundle connectionHint) {
-		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-		currentLocation = client.getLastLocation();
-		locationText.append("\n" + currentLocation.toString());
-		GMapV2Direction md = new GMapV2Direction();
-		LatLng test = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-		LatLng southCampus = new LatLng(southCampusLat, southCampusLong);
-		Document doc = md.getDocument(test, southCampus, GMapV2Direction.MODE_DRIVING);
-		if (doc != null) {
-			ArrayList<LatLng> directionPoint = md.getDirection(doc);
-
-			for (int i = 0; i < directionPoint.size(); i++) {
-					directionText.append("\n" + ((LatLng) directionPoint.get(i)).toString());
-			}
-			Log.d("HUD", doc.toString());
-		}
-	}
-
-	@Override
-	public void onDisconnected() {
-		Toast.makeText(this, "Disconnected. Please re-connect.",
-				Toast.LENGTH_SHORT).show();
-
 	}
 }
