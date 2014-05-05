8c9dbcd8b5832573d07c9119b0cea54c4adc90c6
jtfallon <John_Fallon@student.uml.edu>
2014-05-05 14:54:45 -0400

    Adding in the missing files from the last commit.

---
+++ GPSCarHUD/src/com/uml/gpscarhud/views/TimeToDestinationView.java
@@ -0,0 +1,22 @@
+package com.uml.gpscarhud.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TimeToDestinationView extends View {
+	
+	public TimeToDestinationView(Context context) 
+	{
+		super(context);
+	}
+	public TimeToDestinationView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+	public TimeToDestinationView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+
+}
