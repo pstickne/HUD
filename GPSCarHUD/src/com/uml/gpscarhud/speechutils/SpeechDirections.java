2a0ed3d90eea50e1009cd4fd1651e9d7995f4840
jtfallon <jogtofame5@yahoo.com>
2014-05-05 01:54:38 -0400

    Refined the speech class.

--- GPSCarHUD/src/com/uml/gpscarhud/speechutils/SpeechDirections.java
+++ GPSCarHUD/src/com/uml/gpscarhud/speechutils/SpeechDirections.java
@@ -1,34 +1,38 @@
 package com.uml.gpscarhud.speechutils;
 
+import java.util.Locale;
+
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 
 public class SpeechDirections {
-	
+
 	TextToSpeech speech;
-	
-	public SpeechDirections(Context context)
-	{
+
+	public SpeechDirections(Context context) {
 		speech = new TextToSpeech(context, new OnInitListener() {
-			
+
 			public void onInit(int status) {
-				if( status == TextToSpeech.ERROR)
-					Log.i("SPEECH", "Did not start properly.");				
+				if (status == TextToSpeech.SUCCESS)
+					speech.setLanguage(Locale.getDefault());
+				else
+					Log.i("SPEECH", "Did not start properly.");
 			}
 		});
 	}
-	
-	public void remove()
-	{
+
+	public void remove() {
 		speech.stop();
 		speech.shutdown();
 	}
-	
-	public void say(String message)
-	{
-		speech.speak(message, TextToSpeech.QUEUE_ADD, null);
+
+	public void say(String message, Boolean ensureQueueIsCleared) {
+		if (ensureQueueIsCleared)
+			speech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+		else
+			speech.speak(message, TextToSpeech.QUEUE_ADD, null);
 	}
 
 }
