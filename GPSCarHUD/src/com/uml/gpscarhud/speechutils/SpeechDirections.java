dd8a549a9897c37b29be4eb14e057f7ab8cd4752
jtfallon <jogtofame5@yahoo.com>
2014-05-05 02:03:33 -0400

    Added documentation to the speech class.

--- GPSCarHUD/src/com/uml/gpscarhud/speechutils/SpeechDirections.java
+++ GPSCarHUD/src/com/uml/gpscarhud/speechutils/SpeechDirections.java
@@ -7,6 +7,12 @@ import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 
+/**
+ * @author John This class is to serve as a Singleton for issuing speaking
+ *         instructions. It will be started once the object has been created and
+ *         will be set to the default language of the device.
+ */
+
 public class SpeechDirections {
 
 	TextToSpeech speech;
@@ -16,6 +22,7 @@ public class SpeechDirections {
 
 			public void onInit(int status) {
 				if (status == TextToSpeech.SUCCESS)
+					// Set the language to be used as the device default.
 					speech.setLanguage(Locale.getDefault());
 				else
 					Log.i("SPEECH", "Did not start properly.");
@@ -23,11 +30,21 @@ public class SpeechDirections {
 		});
 	}
 
+	/**
+	 * Call when done with this object to release system resources.
+	 */
 	public void remove() {
 		speech.stop();
 		speech.shutdown();
 	}
 
+	/**
+	 * @param message
+	 *            A string to be spoken by the device.
+	 * @param ensureQueueIsCleared
+	 *            True if it is important that nothing else will be said before
+	 *            this call.
+	 */
 	public void say(String message, Boolean ensureQueueIsCleared) {
 		if (ensureQueueIsCleared)
 			speech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
